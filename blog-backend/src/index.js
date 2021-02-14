fc4a60b8376bdfbc9cd8f6d742978f5a726d4973
parkhojeong <parkhj062@gmail.com>
2021-02-14 20:55:16 +0900

    mongoose

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,6 +1,22 @@
+require("dotenv").config();
+
 const Koa = require("koa");
 const Router = require("koa-router");
 const bodyParser = require("koa-bodyparser");
+
+const mongoose = require("mongoose");
+
+const { PORT: port = 4000, MONGO_URI: mongoURI } = process.env;
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 const api = require("./api");
 
@@ -13,6 +29,6 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log("listening to port 4000");
+app.listen(port, () => {
+  console.log("listening to port", port);
 });
